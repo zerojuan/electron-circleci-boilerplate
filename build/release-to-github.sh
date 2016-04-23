1ae61bf4093ea4ae925dbb0b00333676371b63e0
Julius Cebreros <me@juliuscebreros.com>
2016-04-23 23:39:26 +1200

    Add test and github auth key

--- build/release-to-github.sh
+++ build/release-to-github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
-curl --data "$API_JSON" https://api.github.com/repos/:owner/:repository/releases?access_token=:access_token
+curl --data "$API_JSON" https://api.github.com/repos/zerojuan/electron-circleci-boilerplate/releases?access_token=$GITHUB_AUTH
---
+++ test/index.js
@@ -0,0 +1,8 @@
+const test = require( 'tape' );
+
+test( 'smoketest', function( t ) {
+  // should be a PartyAnimalsData folder
+  t.ok( true,
+    'Auto pass test' );
+  t.end();
+});
