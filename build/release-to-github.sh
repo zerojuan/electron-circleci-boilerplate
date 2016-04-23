57381041392ac45d7e3f888449922b3629aa01b7
Julius Cebreros <me@juliuscebreros.com>
2016-04-24 01:02:19 +1200

    Fix tagname and release name

--- build/release-to-github.sh
+++ build/release-to-github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $CIRCLE_TAG $CIRCLE_TAG $CIRCLE_TAG)
+API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Release of version %s","draft": false,"prerelease": false}' $CIRCLE_TAG $CIRCLE_TAG $CIRCLE_TAG)
 curl --data "$API_JSON" https://api.github.com/repos/zerojuan/electron-circleci-boilerplate/releases?access_token=$GITHUB_AUTH
