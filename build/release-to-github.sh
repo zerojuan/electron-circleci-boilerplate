f01b88ee75666386316ee758d2024680ca47db6b
Julius Cebreros <me@juliuscebreros.com>
2016-04-23 23:53:32 +1200

    Use CircleCI tag variable

--- build/release-to-github.sh
+++ build/release-to-github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $CIRCLE_TAG $CIRCLE_TAG $CIRCLE_TAG)
 curl --data "$API_JSON" https://api.github.com/repos/zerojuan/electron-circleci-boilerplate/releases?access_token=$GITHUB_AUTH
