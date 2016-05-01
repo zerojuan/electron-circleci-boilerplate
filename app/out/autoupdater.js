a7f962c9a340750b62bcf70c44f22f9e8d922f00
Julius Cebreros <me@juliuscebreros.com>
2016-05-01 23:04:32 +1200

    Add update listeners

--- app/out/autoupdater.js
+++ app/out/autoupdater.js
@@ -1,4 +1,31 @@
 const electron = require( 'electron' );
-const autoupdater = electron.autoupdater;
+const os = require( 'os' );
+const autoupdater = electron.autoUpdater;
+const app = electron.app;
 
 const UPDATE_SERVER_HOST = 'sample-electron.juliuscebreros.com';
+
+module.exports = {
+  listenToUpdates: function( window ) {
+    const version = app.getVersion();
+
+    autoupdater.addListener( 'checking-for-update', function() {
+      console.log( 'Checking for update' );
+    });
+
+    autoupdater.addListener( 'update-not-available', function() {
+      console.log( 'Update not available' );
+    });
+
+    autoupdater.addListener( 'update-available', function() {
+      console.log( 'Update available' );
+    });
+
+    autoupdater.setFeedURL( 'https://' + UPDATE_SERVER_HOST +
+      '/update/' + os.platform() + '_' + os.arch() + '/' + version );
+
+    window.webContents.once( 'did-frame-finish-load', function( event ) {
+      autoupdater.checkForUpdates();
+    });
+  }
+};
--- app/out/main.js
+++ app/out/main.js
@@ -8,6 +8,8 @@ const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 const MenuItem = electron.MenuItem;
 
+var updateProcess = require( './autoupdater.js' );
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -34,6 +36,8 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
+
+  updateProcess.listenToUpdates( mainWindow );
 
   // Emitted when the window is closed.
   mainWindow.on( 'closed', function() {
