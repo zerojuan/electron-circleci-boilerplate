b5a8e2f98c3c3f8bc3bb0653d27085c60752b7a8
Julius Cebreros <me@juliuscebreros.com>
2016-05-03 23:03:48 +1200

    Implement notification when upload is ready

--- app/out/autoupdater.js
+++ app/out/autoupdater.js
@@ -1,13 +1,28 @@
 const electron = require( 'electron' );
 const os = require( 'os' );
 const log = require( 'nslog' );
-const autoupdater = electron.autoUpdater;
+const autoUpdater = electron.autoUpdater;
+const BrowserWindow = electron.BrowserWindow;
 const app = electron.app;
+
+const isDev = require( 'electron-is-dev' );
 
 const UPDATE_SERVER_HOST = 'sample-electron.juliuscebreros.com';
 
+function notify( title, message ) {
+  let windows = BrowserWindow.getAllWindows();
+  if ( windows.length === 0 ) {
+    return;
+  }
+
+  windows[ 0 ].webContents.send( 'notify', title, message );
+}
+
 module.exports = {
   listenToUpdates: function( window ) {
+    if ( isDev ) {
+      return;
+    }
     const version = app.getVersion();
 
     if ( os.platform() !== 'darwin' ) {
@@ -16,26 +31,32 @@ module.exports = {
 
     log( 'I am loaded what is up' );
 
-    autoupdater.addListener( 'checking-for-update', function() {
+    autoUpdater.addListener( 'checking-for-update', function() {
       log( 'Checking for update' );
     });
 
-    autoupdater.addListener( 'update-not-available', function() {
+    autoUpdater.addListener( 'update-not-available', function() {
       log( 'Update not available' );
     });
 
-    autoupdater.addListener( 'update-available', function() {
+    autoUpdater.addListener( 'update-available', function() {
       log( 'Update available' );
+    });
+
+    autoUpdater.addListener( 'update-downloaded',
+      function( event, releaseNotes, releaseName, releaseDate, updateURL ) {
+      notify( 'A new update is ready to install',
+        'Version ' + releaseName + ' is downloaded and will be automatically installed on Quit' );
     });
 
     var url = 'http://' + UPDATE_SERVER_HOST +
       '/update/' + os.platform() + '_' + os.arch() + '/v' + version;
     log( url );
-    autoupdater.setFeedURL( url );
+    autoUpdater.setFeedURL( url );
 
     window.webContents.once( 'did-finish-load', function( event ) {
-      log( 'I am checking for updates' );
-      autoupdater.checkForUpdates();
+      autoUpdater.checkForUpdates();
+      notify( 'Checking for updates', 'Yo!' );
     });
 
   }
