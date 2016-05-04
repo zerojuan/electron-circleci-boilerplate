934bda71b37b9cbec7439389213f73e7985c5a12
Julius Cebreros <me@juliuscebreros.com>
2016-05-04 23:06:36 +1200

    Wire startup check to main

--- app/out/main.js
+++ app/out/main.js
@@ -8,6 +8,8 @@ const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 const MenuItem = electron.MenuItem;
 
+const handleStartupEvent = require( './winstartuphandler.js' );
+
 var updateProcess = require( './autoupdater.js' );
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -54,23 +56,25 @@ function createWindow () {
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on( 'ready', createWindow );
+if ( !handleStartupEvent() ) {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  app.on( 'ready', createWindow );
 
-// Quit when all windows are closed.
-app.on( 'window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if ( process.platform !== 'darwin' ) {
-    app.quit();
-  }
-});
+  // Quit when all windows are closed.
+  app.on( 'window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if ( process.platform !== 'darwin' ) {
+      app.quit();
+    }
+  });
 
-app.on( 'activate', function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if ( mainWindow === null ) {
-    createWindow();
-  }
-});
+  app.on( 'activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if ( mainWindow === null ) {
+      createWindow();
+    }
+  });
+}
